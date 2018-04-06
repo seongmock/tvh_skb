21ce695335290fa7a9db3909ea4faf7ec623bd39
sm.yoo <sm.yoo@lge.com>
2018-04-06 16:21:17 +0900

    make_m3u.py modify

--- make_m3u.py
+++ make_m3u.py
@@ -6,4 +6,9 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("Usage> python3 make_m3u.py <filelist>");
         exit();
-    ifile = open(
+    ifile = open(sys.argv[1],"r");
+    found = re.compile('trying to save (\w+://\d+.\d+.\d+.\d+:\d+) to');
+    for line in ifile:
+        match = found.search(line);
+        print(match.group(1));
+        
